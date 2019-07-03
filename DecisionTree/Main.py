4f3c9e5450d46faa5cc57164141938095806d66d
qq893821368 <893821368@qq.com>
2019-07-03 21:16:44 +0800

    finish_Ch.03

--- DecisionTree/Main.py
+++ DecisionTree/Main.py
@@ -2,27 +2,97 @@ from DecisionTree import tree
 from kNN import kNN
 from DecisionTree import treePlotter
 
-data, labels = kNN.file2matrix('testSets/testSet.txt', type='str')
-data = data.tolist()
-for i in range(len(data)):
-    data[i].append(labels[i])
-    for j in range(2):
-        if data[i][j] == 1:
-            data[i][j] = 'y'
-        else:
-            data[i][j] = 'n'
-# labels = ['have house', 'have car', 'number of love']
-labels = ['can fly', 'by oil', 'number of wheels']
-my_tree = tree.create_tree(data, labels)
-labels = ['can fly', 'by oil', 'number of wheels']
-treePlotter.create_plot(my_tree)
 
-print(tree.classify(my_tree, labels, ['y', 'y', 12]))
+# my test function
+def my_test():
+    data, labels = kNN.file2matrix('testSets/testSet.txt', type='str')
+    data = data.tolist()
+    for i in range(len(data)):
+        data[i].append(labels[i])
+        for j in range(2):
+            if data[i][j] == 1:
+                data[i][j] = 'y'
+            else:
+                data[i][j] = 'n'
+    labels = ['can fly', 'by oil', 'number of wheels']
+    my_tree = tree.create_tree(data, labels)
+    labels = ['can fly', 'by oil', 'number of wheels']
+    treePlotter.create_plot(my_tree)
+    print(tree.classify(my_tree, labels, ['y', 'y', 12]))
 
 
-'''
-myDat, labels = tree.create_data_set()
-myTree = treePlotter.retrieve_tree(0)
-print(treePlotter.classify(myTree, labels, [1, 0]))
-print(treePlotter.classify(myTree, labels, [1, 1]))
-'''
+# book's test function
+def book_test():
+    myDat, labels = tree.create_data_set()
+    myTree = treePlotter.retrieve_tree(0)
+    print(treePlotter.classify(myTree, labels, [1, 0]))
+    print(treePlotter.classify(myTree, labels, [1, 1]))
+
+
+def classify_glasses():
+    fr = open('testSets/lenses.txt')
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    lenses_labels = ['age', 'prescript', 'astigmatic', 'tear_rate']
+    lenses_tree = tree.create_tree(lenses, lenses_labels)
+    print('lenses_tree:', lenses_tree)
+    treePlotter.create_plot(lenses_tree)
+    tree.store_tree(lenses_tree, 'trees_repository/tree_for_lenses.txt')
+
+
+def classify_dating():
+    fr = open('testSets/dating.txt')
+    dating = [inst.strip().split('\t') for inst in fr.readlines()]
+    dating_labels = ['age', 'income', 'height']
+    dating_tree = tree.create_tree(dating, dating_labels)
+    print(dating_tree)
+    treePlotter.create_plot(dating_tree)
+
+
+def test_dating():
+    fr = open('testSets/dating_test.txt')
+    dating = [inst.strip().split('\t') for inst in fr.readlines()]
+    dating_labels = ['height', 'income', 'age']
+    for line in dating:
+        del(line[-1])
+    print('dating:')
+    for line in dating:
+        print(line)
+    print('entropy:', tree.calculate_shannon_entropy(dating))
+
+    fr = open('testSets/dating_test2.txt')
+    dating = [inst.strip().split('\t') for inst in fr.readlines()]
+    dating_labels = ['height', 'income', 'age']
+    for line in dating:
+        del (line[-1])
+    print('dating:')
+    for line in dating:
+        print(line)
+    print('entropy:', tree.calculate_shannon_entropy(dating))
+
+    fr = open('testSets/dating.txt')
+    dating = [inst.strip().split('\t') for inst in fr.readlines()]
+    dating_labels = ['height', 'income', 'age']
+    for line in dating:
+        del (line[-1])
+    print('dating:')
+    for line in dating:
+        print(line)
+    print('entropy:', tree.calculate_shannon_entropy(dating))
+
+
+def test_fake():
+    fr = open('testSets/fake_set.txt')
+    fake = [inst.strip().split('\t') for inst in fr.readlines()]
+    fake_labels = ['number']
+    fake_tree = tree.create_tree(fake, fake_labels)
+    print(fake_tree)
+
+
+if __name__ == '__main__':
+    # test_fake()
+    classify_glasses()
+    # my_test()
+    # classify_dating()
+    # test_dating()
+
+
--- DecisionTree/tree.py
+++ DecisionTree/tree.py
@@ -9,6 +9,18 @@ def calculate_shannon_entropy(data_set):
     :param data_set: 数据集, list形式
     :return: 香农熵, float
     """
+    """
+    ┌                       ┐
+        e11, e12, e13, e14
+        e21, e22, e23, e24
+        e31, e32, e33, e34
+        e41, e42, e43, e44
+    └                       ┘
+    对上述矩阵, 取出对每一行的最后一位元素, 
+    放入一个dict, e_name: frequency, 
+    即可计算每个元素的出现概率并最后得到香农熵
+    香农熵公式: -∑ Pi·log(Pi) [i=1,2,3, ... ,n]
+    """
     num_entries = len(data_set)
     label_counts = {}
     for feat_vec in data_set:
@@ -50,6 +62,23 @@ def split_data_set(data_set, axis, value):
     :param value: 特征值, type(data_set[axis])
     :return: 分割结果集, list形式
     """
+    """
+        如果axis列上的值全与value相等, 则相当于从矩阵中删除某一列, 如下图
+        ┌                       ┐               ┌                    ┐
+            e11, e12, e13, e14                        e11, e12, e14
+            e21, e22, e23, e24                        e21, e22, e24
+            e31, e32, e33, e34              ->        e31, e32, e34
+            e41, e42, e43, e44                        e41, e42, e44
+        └                       ┘               └                    ┘
+        如果axis列上的值不全与value相等, 则会剩下部分剔除了axis列的行, 如下图
+        ┌                       ┐               
+            e11, e12, e13, e14                    ┌                    ┐
+            e21, e22, e23, e24                        e11, e12, e14
+            e31, e32, e33, e34              ->        e31, e32, e34
+            e41, e42, e43, e44                        e41, e42, e44
+        └                       ┘               └                    ┘
+        最后剩下的行, axis列的值必须与value相等
+        """
     ret_data_set = []
     for feat_vec in data_set:
         if feat_vec[axis] == value:
@@ -98,6 +127,15 @@ def majority_count(class_list):
     return sorted_class_count[0][0]
 
 
+def prob_class(class_list):
+    class_count = {}
+    for vote in class_list:
+        class_count[vote] = class_count.get(vote, 0) + 1
+    for key in class_count:
+        class_count[key] = class_count[key] / len(class_list)
+    return class_count
+
+
 def create_tree(data_set, labels):
     """
     创建决策树节点
@@ -106,23 +144,37 @@ def create_tree(data_set, labels):
     :param labels: 标签集, list形式
     :return:决策树节点, dict形式/str
     """
+    """
+    有时, 返回的决策树会没有用上所有的特征来分割,
+    这是因为有的特征在数据中的确对数据的分类没有影响,
+    这也是该算法的好处之一, 即便数据有冗余的属性,
+    仍然可以准确地生成决策树, 起效的正是第一个判断-
+    -语句, 如果所有数据已经是同类, 则不计其余特征
+    """
     class_list = [example[-1] for example in data_set]
     if class_list.count(class_list[0]) == len(class_list):  # 如果所有的数据都为同一类. 直接结束, 返回该类型
         return class_list[0]
     if len(data_set[0]) == 1:  # 如果只剩下一个特征, 返回出现最多次的类型
-        return majority_count(class_list)
+        return prob_class(class_list)
+    # 这个条件语句是用来处理只有类别之分的数据, 此时data_set应被视为无效数据
+    # 因为只有类别却没有属性值用来分割, 没有判别类型的依据
     best_feature = choose_best_feature_to_split(data_set)
     best_feature_label = labels[best_feature]  # 最佳分割特征标签
     my_tree = {best_feature_label: {}}  # 创建决策树节点, 格式为{特征标签 : 归属于该类的子树}
     del(labels[best_feature])  # 删除最佳分割标签, 剩余子集好进行下一步选择新最佳分割标签 (这一步会对原list进行操作导致原list变小)
     # del不是直接从内存中删除, 而是删除这个指针引用, 此处等效于labels.remove(best_feature_label)
-    feature_values = [example[best_feature] for example in data_set]  # 把每条数据的最佳分割特征值取出
+    feature_values = [example[best_feature] for example in data_set]  # 把每条数据的最佳分割特征值取出组成一个列表
     unique_vals = set(feature_values)  # 最佳分割特征值集合
     for value in unique_vals:  # 在集合里, 决策树节点[最佳特征标签][最佳分割特征值] = 子决策树节点
         sub_labels = labels[:]  # 因为每一个决策树节点之后, 可能是直接判别类别, 也可能是继续判断的一个子节点
         my_tree[best_feature_label][value] = create_tree(split_data_set(data_set, best_feature, value), sub_labels)
         # 所以, 返回的决策树节点可以是dict也可以是str, 那么就调用本方法继续生成下一个节点
         # 递归地调用本方法, 通过返回新的节点给上一层的父节点, 最终生成一个根节点作为决策树的入口节点
+        # # 这里注意, 每个节点下, 对best_feature_label这个key, 每种value应该都有一个子节点, 由本方法生成
+        # # 所以, 这里是一个循环, 对最佳分割特征中每一个独特的特征值都要调用一次本方法,
+        # # 并且是用以该value分割好的子data_set去新生成一个子节点
+        # # 因为这里根节点已经是对某一feature来进行分割, 在这一feature下的每个feature值, 都对应了一个子data_set
+        # # 对于这个子data_set还需要用同样的方法进行分割, 形成一颗完成的子树作为子节点嵌入根节点
     return my_tree
 
 
